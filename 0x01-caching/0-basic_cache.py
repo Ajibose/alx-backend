4842d72f0cb7cf7f81a729c89a6d10ae473465c7
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-03 12:40:05 +0100

    Task0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,53 +1,21 @@
-#!/usr/bin/env python3
-"""Create a class BasicCache that inherits from
-BaseCaching and is a caching system:
-
-You must use self.cache_data - dictionary from
-the parent class BaseCaching
-This caching system doesn’t have limit
-def put(self, key, item):
-Must assign to the dictionary self.cache_data the
-item value for the key key.
-If key or item is None, this method should not do
-anything.
-def get(self, key):
-Must return the value in self.cache_data linked
-to key.
-If key is None or if the key doesn’t exist in
-self.cache_data, return None.
+#!/usr/bin/python3
 """
-
-
-BaseCaching = __import__('base_caching').BaseCaching
+    Create a caching system class that inherits from BasicCache
+"""
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """_summary_
-    """
-
-    def __init__(self):
-        """_summary_
-        """
-        super().__init__()
-
     def put(self, key, item):
-        """_summary_
+        """Add an item in the cache"""
+        if not key or not item:
+            return
 
-        Args:
-                key (_type_): _description_
-                item (_type_): _description_
-        """
-        if key is None or item is None:
-            pass
-        else:
-            self.cache_data[key] = item
+        self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
-
-        Args:
-                key (_type_): _description_
-        """
-        if key is None or key not in self.cache_data.keys():
+        """Get an item by key from the cache"""
+        if not key or key not in self.cache_data:
             return None
-        return self.cache_data.get(key)
+
+        return self.cache_data[key]
