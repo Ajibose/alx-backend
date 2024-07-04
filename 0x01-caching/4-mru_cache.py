24988759d95b251396c64e8721070c7a91815306
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-04 13:58:41 +0100

    Task4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,25 +1,7 @@
-#!/usr/bin/env python3
-"""Create a class MRUCache that inherits
-from BaseCaching and is a caching system:
-
-You must use self.cache_data - dictionary
-from the parent class BaseCaching
-You can overload def __init__(self): but don’t
-forget to call the parent init: super().__init__()
-def put(self, key, item):
-Must assign to the dictionary self.cache_data the
-item value for the key key.
-If key or item is None, this method should not do anything.
-If the number of items in self.cache_data is higher
-that BaseCaching.MAX_ITEMS:
-you must discard the most recently used item
-(MRU algorithm)
-you must print DISCARD: with the key discarded and
-following by a new line
-def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in
-self.cache_data, return None.
+#!/usr/bin/python3
+"""Create a class MRUCache
+inherits from BaseCaching and is a caching system
+You must use self.cache_data - dictionary from the parent class BaseCaching
 """
 
 
@@ -27,41 +9,30 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """_summary_
+    """A Most Recently Used Cache system
+    Leverage python's inherent dictionary order
     """
-
     def __init__(self):
-        """_summary_
-        """
+        """Initialize and set Cache"""
         super().__init__()
-        self.usedKeys = []
 
     def put(self, key, item):
-        """_summary_
+        """Put or Update an item in the Cache"""
+        if key is None or item is None:
+            return
 
-        Args:
-                        key (_type_): _description_
-                        item (_type_): _description_
-        """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            if key not in self.usedKeys:
-                self.usedKeys.append(key)
-            else:
-                self.usedKeys.append(
-                    self.usedKeys.pop(self.usedKeys.index(key)))
-            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(-2)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+        if key in self.cache_data:
+            del self.cache_data[key]
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            recent_key, recent_value = self.cache_data.popitem()
+            print(f"DISCARD: {recent_key}")
+
+        self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
-
-        Args:
-                        key (_type_): _description_
-        """
-        if key is not None and key in self.cache_data.keys():
-            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
-            return self.cache_data.get(key)
-        return None
+        """Get an item from the cache"""
+        item = self.cache_data.get(key, None)
+        if item:
+            self.put(key, item)
+        return item
